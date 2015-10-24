3df44ee5341c15a877a3307f27669796903bc0b2
Jarek Kaspar <kaspar.jarek@gmail.com>
2015-10-24 22:49:50 +0200

    Basic implementation of tank.cpp file

--- src/tank.cpp
+++ src/tank.cpp
@@ -1,4 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+#include <syslog.h>
+
+
 int main(void)
 {
-	return 0;
+    syslog(LOG_INFO, "Tank with pid %d was created\n", getpid());
+    sleep(2);
+    return 0;
 }
