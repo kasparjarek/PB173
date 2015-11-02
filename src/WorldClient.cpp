2a0b2777b652632312e5c5d2c6a87b077bcb7d67
Dhrung <m4medek@gmail.com>
2015-11-02 17:52:27 +0100

    added worldclient, jeste nutno dodelat

---
+++ src/WorldClient.cpp
@@ -0,0 +1,5 @@
+//
+// Created by med on 30.10.15.
+//
+
+#include "WorldClient.h"
---
+++ src/WorldClient.h
@@ -0,0 +1,14 @@
+//
+// Created by med on 30.10.15.
+//
+
+#ifndef INTERNET_OF_TANKS_WORLDCLIENT_H
+#define INTERNET_OF_TANKS_WORLDCLIENT_H
+
+
+class WorldClient {
+
+};
+
+
+#endif //INTERNET_OF_TANKS_WORLDCLIENT_H
