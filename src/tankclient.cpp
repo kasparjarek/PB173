27e0341dc157f141b79aaa009da1c2aa52103a4a
Jarek Kaspar <kaspar.jarek@gmail.com>
2015-12-13 01:27:13 +0100

    Tank client read from stdin, receive and send udp packets

--- src/tankclient.cpp
+++ src/tankclient.cpp
@@ -4,9 +4,10 @@
 #include <sys/socket.h>
 #include <netdb.h>
 #include <syslog.h>
-#include <cstring>
+#include <ncurses.h>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 const char *IOT_PORT = "1337";
 const char *ARGS = "i:h";
@@ -16,19 +17,118 @@ const struct option LONG_ARGS[] = {
     {0, 0, 0, 0}
 };
 
+int sendCmdCurrentLine = 0;
+int recvCmdCurrentLine = 0;
+int sockfd;
+
 void printHelp()
 {
     cout << "\t" << "-i, --ip-address <ipaddr>" << endl;
-    cout << "\t\t" << "ip address of the server (default is localhost" << endl;
+    cout << "\t\t" << "ip address of the server (default is localhost)" << endl;
 
     cout << "\t" << "-h, --help" << endl;
     cout << "\t\t" << "shows this help" << endl << endl;
+
+    cout << "\t" << "How to control the tank:" << endl;
+    cout << "\t\t" << "Use 'q' for exit" << endl;
+    cout << "\t\t" << "Use 'w' 'a' 's' 'd' for movement." << endl;
+    cout << "\t\t" << "Use arrows for shooting." << endl << endl;
+}
+
+void sendMsg(char *msg)
+{
+    attron(COLOR_PAIR(1));
+
+    ssize_t rv = send(sockfd, msg, 2, MSG_DONTWAIT);
+    if (rv == 2) {
+        syslog(LOG_INFO, "send() sent msg: %s", msg);
+    }
+    else if (rv == -1) {
+        syslog(LOG_ERR, "send() failed: %s", strerror(errno));
+        attron(COLOR_PAIR(3));
+    }
+    else {
+        syslog(LOG_ERR, "send() sent %d chars instead of %d", (int) rv, 2);
+    }
+}
+
+void printSendCmd(char *msg)
+{
+    if (sendCmdCurrentLine == LINES) {
+        clear();
+        sendCmdCurrentLine = 0;
+        recvCmdCurrentLine = 0;
+    }
+    mvprintw(sendCmdCurrentLine++, 0, "Sending command: %s", msg);
+}
+
+void printRecvCmd(char *msg) {
+    attron(COLOR_PAIR(2));
+    if (recvCmdCurrentLine == LINES) {
+        clear();
+        sendCmdCurrentLine = 0;
+        recvCmdCurrentLine = 0;
+    }
+    mvprintw(recvCmdCurrentLine++, COLS / 2, "World received: %s", msg);
+}
+
+int readInput()
+{
+    int input = getch();
+    switch (input){
+
+        // Quit
+        case 'q':
+            return -1;
+
+        // Send move actions
+        case 'w':
+            sendMsg("mu");
+            printSendCmd("Move Up");
+            break;
+        case 's':
+            sendMsg("md");
+            printSendCmd("Move Down");
+            break;
+        case 'a':
+            sendMsg("ml");
+            printSendCmd("Move Left");
+            break;
+        case 'd':
+            sendMsg("mr");
+            printSendCmd("Move Right");
+            break;
+
+        // Send fire actions
+        case KEY_LEFT:
+            sendMsg("fl");
+            printSendCmd("Fire Left");
+            break;
+        case KEY_RIGHT:
+            sendMsg("fr");
+            printSendCmd("Fire Right");
+            break;
+        case KEY_UP:
+            sendMsg("fu");
+            printSendCmd("Fire Up");
+            break;
+        case KEY_DOWN:
+            sendMsg("fd");
+            printSendCmd("Fire Down");
+            break;
+
+        // In case of none or unknown input do nothing
+        default:
+            break;
+    }
+
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     char opt = 0;
-    string ip_address = "127.0.0.1";
+    std::string ip_address = "127.0.0.1";
 
     while ((opt = getopt_long(argc, argv, ARGS, LONG_ARGS, NULL)) != -1) {
         switch (opt) {
@@ -44,24 +144,27 @@ int main(int argc, char *argv[])
         }
     }
 
-    int rv;
+    // Prepare server info
+
     struct addrinfo hints;
     struct addrinfo *servinfo;
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_INET;
     hints.ai_socktype = SOCK_DGRAM;
 
+    int rv;
     if ((rv = getaddrinfo(ip_address.c_str(), IOT_PORT, &hints, &servinfo)) != 0) {
         syslog(LOG_ERR, "getaddrinfo() failed: %s", gai_strerror(rv));
         exit(1);
     }
 
-    int sockfd;
+    // Create socket
+
     struct addrinfo *p;
     for (p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
-            continue;
-        break;
+        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) != -1) {
+            break;
+        }
     }
 
     if (p == NULL) {
@@ -69,8 +172,45 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    /* sendto sockfd */
+    // Make Connection
 
+    if (connect(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+        syslog(LOG_ERR, "connect() failed: %s", strerror(errno));
+    }
+
+    // Start ncurses
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    curs_set(0);
+    timeout(500);
+
+    start_color();
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
+    init_pair(2, COLOR_GREEN, COLOR_BLACK);
+    init_pair(3, COLOR_RED, COLOR_BLACK);
+
+    // Listen for input and receive msg from socket
+    char buff[3];
+    buff[2] = 0;
+    while (readInput() == 0) {
+
+        ssize_t recvRet = recv(sockfd, buff, 2, MSG_DONTWAIT);
+
+        if (recvRet == -1) {
+            syslog(LOG_ERR, "recv() failed: %s", strerror(errno));
+        }
+        else if (recvRet != 2) {
+            syslog(LOG_ERR, "recv() read %d chars instead of %d", (int) recvRet, 2);
+        }
+        else {
+            printRecvCmd(buff);
+        }
+    }
+
+    endwin();
+    close(sockfd);
     freeaddrinfo(servinfo);
     exit(0);
 }
